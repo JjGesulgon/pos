142b4f947af3599d10bce6a60006d1935aa4e7dd
jjgesulgon <jj@hybrain.co>
2020-04-26 02:35:26 +0800

    UI Update POS

--- resources/js/views/layouts/Navbar.vue
+++ resources/js/views/layouts/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
         <div class="container-fluid">
-            <router-link class="navbar-brand" :to="{ name: 'overview' }">Wilnor Carshop</router-link>
+            <router-link class="navbar-brand" :to="{ name: 'overview' }">POS</router-link>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
--- resources/js/views/layouts/Sidebar.vue
+++ resources/js/views/layouts/Sidebar.vue
@@ -1,30 +1,52 @@
 <template>
-    <div class="sticky-top">
-        <ul class="nav flex-column background py-1 mt-3 sidebar-nav">
-            <li class="nav-item">
-                <span class="nav-link">Dashboards</span>
-            </li>
+    <div>
+        <div class="sticky-top">
+            <ul class="nav flex-column background py-1 mt-3 sidebar-nav">
+                <li class="nav-item">
+                    <router-link class="nav-link" :to="{ name: 'overview' }"><i class="fas fa-home"></i>&nbsp; Overview</router-link>
+                </li>
+            </ul>
+        </div>
 
-            <li class="nav-item">
-                <router-link class="nav-link" :to="{ name: 'overview' }">Overview</router-link>
-            </li>
+        <div class="sticky-top">
+            <ul class="nav flex-column background py-1 mt-3 sidebar-nav">
+                <li class="nav-item">
+                    <span class="nav-link"><i class="fas fa-briefcase"></i>&nbsp; Business Functions</span>
+                </li>
 
-            <li class="nav-item">
-                <a class="nav-link" href="#users" data-toggle="collapse" aria-expanded="false" aria-controls="users">
-                    Users <i class="float-right fas fa-caret-down"></i>
-                </a>
-            </li>
-            <div class="collapse" id="users">
-                <ul class="nav flex-column nav-sub-menu">
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'users.create' }">Create New User</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'users.index' }">View Users</router-link>
-                    </li>
-                </ul>
-            </div>
-        </ul>
+                <li class="nav-item">
+                    <router-link class="nav-link" :to="{ name: 'overview' }">Items</router-link>
+                </li>
+
+                <li class="nav-item">
+                    <router-link class="nav-link" :to="{ name: 'overview' }">Transactions</router-link>
+                </li>
+            </ul>
+        </div>
+
+        <div class="sticky-top">
+            <ul class="nav flex-column background py-1 mt-3 sidebar-nav">
+                <li class="nav-item">
+                    <span class="nav-link"><i class="fas fa-user"></i>&nbsp; Manage Users</span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#users" data-toggle="collapse" aria-expanded="false" aria-controls="users">
+                        Users <i class="float-right fas fa-caret-down"></i>
+                    </a>
+                </li>
+                <div class="collapse" id="users">
+                    <ul class="nav flex-column nav-sub-menu">
+                        <li class="nav-item">
+                            <router-link class="nav-link" :to="{ name: 'users.create' }">Create New User</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" :to="{ name: 'users.index' }">View Users</router-link>
+                        </li>
+                    </ul>
+                </div>
+                
+            </ul>
+        </div>
     </div>
 </template>
 
