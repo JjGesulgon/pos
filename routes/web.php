652f0a4d86cd5c82a59f6f3c493912c45f4ab03e
skeeith <gether.medel@gmail.com>
2020-07-12 15:15:23 +0800

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,25 @@
 <?php
 
 // Login
-Route::get('/login', 'Auth\LoginController@showLoginForm');
-Route::post('/login', 'Auth\LoginController@login')->name('login');
-Route::post('/logout', 'Auth\LoginController@logout');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
 
 // Forget Password
-Route::get('/password/reset/', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('/password/email/', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 
 // Reset Password
-Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('show-password-reset-form');
-Route::post('/password/reset/', 'Auth\ResetPasswordController@reset')->name('password.reset');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 // Register
-Route::get('register/', 'Auth\RegisterController@showRegistrationForm')->name('show-registration-form');
-Route::post('register/', 'Auth\RegisterController@register')->name('register');
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Confirmation
+Route::get('password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
+Route::post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
 
 // Spa
-Route::get('/{any}', 'SpaController@index')->where('any', '.*')->name('index');
+Route::get('{any}', 'SpaController@index')->where('any', '.*')->name('index');
